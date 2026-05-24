43d0c457071cd5173979a7b235acf3773b8e89cf
cristhianbarrientoss-svg <cristhian.barrientoss@unmsm.edu.pe>
2026-05-24 15:29:44 +0000

    declaracion de variables

--- codigo/programa.pas
+++ codigo/programa.pas
@@ -2,39 +2,34 @@ program SistemaGestionAcademica;
 
 uses crt;
 
-type 
+type
+    
+    DatosCurso = record
+        nombreCurso: string[50];
+        profesor: string[50];
+        nota: real;
+        peso: integer; // Créditos de la materia
+    end;
 
-    DatosCurso= record
-    nombreCurso: string[50];
-    profesor: string[50];
-    nota: real;
-    peso: integer;
-end;
+    
+    DatosAlumno = record
+        nombreEstudiante: string[50];
+        cursos: array[1..10] of DatosCurso; 
+        cantidadCursos: integer;
+        promedioPonderado: real;
+    end;
 
-DatosAlumno= record
-    nombreEstudiante: string[50];
-    cursos: array[1..10] of DatosCurso;
-    cantidadCursos: integer;
-    promedioPonderado: real;
-end;
+    
+    RegistroProfesor = record
+        nombreProf: string[50];
+        sumaNotas: real;
+        totalNotas: integer;
+        aprobados: integer;
+        desaprobados: integer;
+        promedioNotas: real;
+    end;
 
-var 
+var
+    
     salon: array[1..100] of DatosAlumno;
     cantidadAlumnos: integer;
-
-
-
-procedure RegistrarAlumnos;
-begin
-    clrscr;
-    writeln('========================================');
-    writeln('      REGISTRO DE ESTUDIANTES           ');
-    writeln('========================================');
-
-    writeln('Espacio reservado para tu codigo de lectura...');
-
-    writeln;
-    writeln('Presione cualquier tecla para continuar...');
-    readkey;
-
-end;
