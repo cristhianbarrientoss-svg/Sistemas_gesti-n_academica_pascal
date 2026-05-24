c9a71a3539e3c15957e14eb4b75bb2ec463b1915
Jesus-alache <juan.alachec@unmsm.edu.pe>
2026-05-24 16:07:16 +0000

    Registramos los datos de alumnos curso, profesores, credito y notas, en los arreglos

--- codigo/programa.pas
+++ codigo/programa.pas
@@ -38,4 +38,39 @@ var
 {      PARTE 1: REGISTRO DE ALUMNOS Y NOTAS                                    }
 { ============================================================================ }
 
-asafbashkbashcashdashdbh
+    write('¿Cuántos alumnos deseas registrar en total?: ');
+    readln(cantidadAlumnos);
+    //Iniciamos con un for para obtener los datos segun la cantidad de alumnos
+    for i := 1 to cantidadAlumnos do
+    begin
+        clrscr; //este procedimiento sirve para limpiar la pantalla
+        writeln('=============================================');
+        writeln('           DATOS DEL ALUMNO #', i);
+        writeln('=============================================');
+        write('Nombre completo del estudiante: ');
+        readln(salon[i].nombreEstudiante);
+        
+        write('¿Cuántos cursos lleva ', salon[i].nombreEstudiante, '?: ');
+        readln(salon[i].cantidadCursos);
+        
+        for j := 1 to salon[i].cantidadCursos do
+        begin
+            writeln;
+            writeln('-> Curso #', j, ' para ', salon[i].nombreEstudiante, ':');
+            write('   Nombre del curso: ');
+            readln(salon[i].cursos[j].nombreCurso);
+            write('   Nombre del profesor: ');
+            readln(salon[i].cursos[j].profesor);
+            write('   Nota obtenida (0 - 20): ');
+            readln(salon[i].cursos[j].nota);
+            write('   Peso/Créditos del curso: ');
+            readln(salon[i].cursos[j].peso);
+        end;
+    end;
+    
+    writeln;
+    writeln('=============================================');
+    writeln('¡Todos los alumnos y notas guardados con éxito!');
+    writeln('Presione cualquier tecla para regresar al menu...');
+    readkey; //procedimiento para que el usuario presione una tecla y regresa al menu
+end;
