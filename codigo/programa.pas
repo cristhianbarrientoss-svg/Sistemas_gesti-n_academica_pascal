b492503fd4aa23d00979ba71404de5458783f07d
cristhianbarrientoss-svg <cristhian.barrientoss@unmsm.edu.pe>
2026-05-24 14:20:56 +0000

    avance de registro de estudiantes

--- codigo/programa.pas
+++ codigo/programa.pas
@@ -10,3 +10,31 @@ type
     nota: real;
     peso: integer;
 end;
+
+DatosAlumno= record
+    nombreEstudiante: string[50];
+    cursos: array[1..10] of DatosCurso;
+    cantidadCursos: integer;
+    promedioPonderado: real;
+end;
+
+var 
+    salon: array[1..100] of DatosAlumno;
+    cantidadAlumnos: integer;
+
+
+
+procedure RegistrarAlumnos;
+begin
+    clrscr;
+    writeln('========================================');
+    writeln('      REGISTRO DE ESTUDIANTES           ');
+    writeln('========================================');
+
+    writeln('Espacio reservado para tu codigo de lectura...');
+
+    writeln;
+    writeln('Presione cualquier tecla para continuar...');
+    readkey;
+
+end;
